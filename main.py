474f93929758623ccbeb59124feb502936fa2f44
k04b <117716296+k04b@users.noreply.github.com>
2025-09-02 16:27:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from editor_window import EditorWindow
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = EditorWindow()
+    window.show()
+    sys.exit(app.exec())
